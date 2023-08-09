bd80f5226b86201368aca6a6362156a360ac37c2
kim jong hwa <ehdxka3@naver.com>
2023-08-09 14:44:50 +0900

    댓글 저장 기능

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
 app = Flask(__name__)
 
-from pymongo import MongoClient
 import certifi
-
 ca=certifi.where()
 
-client = MongoClient("mongodb+srv://sparta:test@cluster0.mcd1ews.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
-db = client.dbsparta_plus_week4
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.s6ifo6t.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -50,6 +48,25 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+@app.route('/api/comment', methods=["POST"])
+def comment_post():
+    id_receive = request.form['id_give']
+    comment_receive = request.form['comment_give']
+    star_receive = request.form['star_give']
+
+    doc = {
+        'id' : id_receive,
+        'comment' : comment_receive,
+        'star' : star_receive
+    }
+
+    db.comment.insert_one(doc)
+    return jsonify({'msg': '맛있다!'})
 
 
 #################################
