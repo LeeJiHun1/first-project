be63bde81d8a973c8b9769ddb351c6b8eb251c07
kim jong hwa <ehdxka3@naver.com>
2023-08-09 16:54:33 +0900

    리뷰 기능 권한 부여

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
 app = Flask(__name__)
 
-from pymongo import MongoClient
 import certifi
-
 ca=certifi.where()
 
-client = MongoClient("mongodb+srv://sparta:test@cluster0.mcd1ews.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
-db = client.dbsparta_plus_week4
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.s6ifo6t.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -51,6 +49,39 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+@app.route('/api/comment', methods=["POST"])
+def comment_post():
+    token_receive = request.headers.get('Authorization')
+
+    if token_receive:
+        try:
+            id_receive = request.form['id_give']
+            comment_receive = request.form['comment_give']
+            star_receive = request.form['star_give']
+
+            doc = {
+                    'id' : id_receive,
+                    'comment' : comment_receive,
+                    'star' : star_receive
+                }
+            db.comment.insert_one(doc)
+            return jsonify({'result': 'success', 'msg': '맛있다!'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+        except jwt.exceptions.DecodeError:
+            return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+    else:
+        return jsonify({'result': 'fail', 'msg': '토큰이 필요합니다.'})
+
+@app.route('/api/comment', methods=["GET"])
+def comment_get():
+    all_comment = list(db.comment.find({}, {'_id': False}))
+    return jsonify({'result':all_comment})
+
 
 #################################
 ##  로그인을 위한 API            ##
@@ -70,6 +101,8 @@ def api_register():
     db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
 
     return jsonify({'result': 'success'})
+
+
 
 
 # [로그인 API]
@@ -93,7 +126,7 @@ def api_login():
         # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=600)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         # token을 줍니다.
